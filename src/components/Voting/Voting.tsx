c39eead751ebe599cf8fa1fc0496de8aefec1526
rsvistel <romann.kravets@gmail.com>
2024-02-10 19:33:44 +0200

    add start

---
+++ src/components/Voting/Voting.tsx
@@ -0,0 +1,38 @@
+
+export default function Voting({regionData, setRegionData, getFormApp}) {
+  let setValueData = (valueToUpdate) => {
+    const updatedRegionData = { ...regionData };
+    updatedRegionData.value = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0
+    };
+    updatedRegionData.value = {
+      ...updatedRegionData.value,
+      ...valueToUpdate
+    };
+    setRegionData(updatedRegionData);
+  };
+
+  return (
+    <main className="voutingName">
+      <button onClick={() => {
+        getFormApp(regionData.region)
+        setValueData( {1: 1});
+      }}>1</button>
+      <button onClick={() => {
+        getFormApp(regionData.region)
+        setValueData( {2: 1});
+      }}>2</button>
+      <button onClick={() => {
+        getFormApp(regionData.region)
+        setValueData( {3: 1});
+      }}>3</button>
+      <button onClick={() => {
+        getFormApp(regionData.region)
+        setValueData( {4: 1});
+      }}>4</button>
+    </main>
+  );
+}
