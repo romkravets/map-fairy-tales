4090942c4205748578fcd5a345d1a4a323537072
rsvistel <romann.kravets@gmail.com>
2024-02-26 11:47:09 +0200

    fix deploy

--- src/components/Auth/AuthStateWrapper.js
+++ src/components/Auth/AuthStateWrapper.js
@@ -33,7 +33,7 @@ const AuthStateWrapper = ({ children }) => {
 
     const unsubscribe = auth.onAuthStateChanged(handleAuthStateChanged)
     return () => unsubscribe()
-  }, [])
+  }, [contextValue])
 
   return <>{children}</>
 }
