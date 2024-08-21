1ea18b8d93f69c4e56036d949110b75c7f00360f
rsvistel <romann.kravets@gmail.com>
2024-08-22 00:04:20 +0300

    map style

--- src/components/MapWorld3/MapWorld3.tsx
+++ src/components/MapWorld3/MapWorld3.tsx
@@ -1,184 +1,8 @@
-// 'use client'
-// import React, {FC, useEffect, useId, useRef, useState} from "react";
-// import { scaleLinear } from "d3-scale";
-// import { useRouter } from 'next/navigation'
-//
-// import {
-//   ComposableMap,
-//   Geographies,
-//   Geography,
-//   ZoomableGroup,
-//   Marker,
-//   Annotation,
-// } from "react-simple-maps";
-// import * as d3 from "d3-geo"; // For calculating the centroid
-//
-//
-// import { FaPlus, FaMinus } from "react-icons/fa6";
-// import { BiTargetLock } from "react-icons/bi";
-//
-// const geoUrl = "/features.json";
-//
-// const countryImages = [
-//   {
-//     id: "USA",
-//     name: "United States",
-//     coordinates: [-100.0, 40.0], // Longitude, Latitude
-//     imageUrl: "/assets/map-icon/usa.png", // Path to the image
-//   },
-//   {
-//     id: "UK",
-//     name: "United Kingdom",
-//     coordinates: [-3.0, 55.0],
-//     imageUrl: "/assets/map-icon/usa.png", // Path to the image
-//   },
-//   // Add more countries as needed
-// ];
-//
-// const MapWorld3 = () => {
-//
-//   const router = useRouter()
-//   const [randomColor, setRandomColor] = useState([])
-//
-//   const handleClick = (geo: any) => () => {
-//     const region = geo.properties.name;
-//     const id = geo.id;
-//     console.log(region, id)
-//     router.push(`/stories?region=${region}&id=${id}`)
-//   };
-//
-//   const getRandomColor = () => {
-//     const letters = '0123456789ABCDEF';
-//     let color = '#';
-//     for (let i = 0; i < 6; i++) {
-//       color += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return color;
-//   };
-//
-//   useEffect(() => {
-//     const countRandomColor = 3;
-//     setRandomColor(getRandomColor());
-//     for (let i = 1; i <= countRandomColor; i++) {
-//       setTimeout(() => {
-//         setRandomColor(getRandomColor());
-//       }, 2000 * i); // Delay increases by 2 seconds each time
-//     }
-//   }, [])
-//
-//
-//   return (
-//     <>
-//       <ComposableMap
-//         projectionConfig={{
-//           rotate: [-10, 0, 0],
-//           scale: 147,
-//         }}
-//         width={800}
-//         height={400}
-//         style={{ backgroundColor: "#F0F8FF" }}
-//       >
-//         <ZoomableGroup>
-//           <Geographies geography={geoUrl}>
-//             {({ geographies }) =>
-//               geographies.map((geo) => {
-//                 const regionCode = geo.id;
-//                 const regionName = geo.properties.name;
-//                 const centroid = d3.geoCentroid(geo); // This gi
-//                 console.log(regionName, centroid)
-//                 //const voteValue = usersVoting[regionCode] || 0;
-//                 return (
-//                   <React.Fragment key={regionCode}>
-//                   <Geography
-//                     geography={geo}
-//                     fill={randomColor} // Sky Blue color
-//                     onClick={handleClick(geo)}
-//                     //onMouseEnter={(event) => handleMouseEnter(geo, event)}
-//                     //onMouseLeave={handleMouseLeave}
-//                     style={{
-//                       default: {
-//                         outline: "none",
-//                         cursor: "pointer"
-//                       },
-//                       hover: {
-//                         outline: "none",
-//                         fill: "#6495ED",
-//                         cursor: "pointer"
-//                       },
-//                       pressed: {
-//                         outline: "none",
-//                         fill: "#1E90FF",
-//                       },
-//                     }}
-//                   >
-//                     {/*<Annotation*/}
-//                     {/*  subject={centroid}*/}
-//                     {/*  dx={-20}*/}
-//                     {/*  dy={-10}*/}
-//                     {/*  connectorProps={{*/}
-//                     {/*    stroke: "#FF5533",*/}
-//                     {/*    strokeWidth: 1,*/}
-//                     {/*    strokeLinecap: "round",*/}
-//                     {/*  }}*/}
-//                     {/*>*/}
-//                     {/*  <text*/}
-//                     {/*    x={4}*/}
-//                     {/*    fontSize={10}*/}
-//                     {/*    alignmentBaseline="middle"*/}
-//                     {/*    fill="#FF5533"*/}
-//                     {/*  >*/}
-//                     {/*    {regionName}*/}
-//                     {/*  </text>*/}
-//                     {/*</Annotation>*/}
-//                       <Marker
-//                         // subject={centroid}
-//                         // dx={-20}
-//                         // dy={-10}
-//                         // connectorProps={{
-//                         //   stroke: "#FF5533",
-//                         //   strokeWidth: 1,
-//                         //   strokeLinecap: "round",
-//                         // }}
-//                         coordinates={centroid}
-//                       >
-//                         {/*<rect x={0} y={0} width={200} height={100} fill="#EEE" />*/}
-//                         <text
-//                           x={4}
-//                           fontSize={10}
-//                           alignmentBaseline="middle"
-//                           fill="#0000"
-//                         >
-//                           {regionName}
-//                         </text>
-//                       </Marker>
-//                   </Geography>
-//                   </React.Fragment>
-//                 );
-//               })
-//             }
-//           </Geographies>
-//           {/*{countryImages.map(({ id, coordinates, imageUrl }) => (*/}
-//           {/*  <Marker key={id} coordinates={coordinates}>*/}
-//           {/*    <image*/}
-//           {/*      href={imageUrl}*/}
-//           {/*      width="20"*/}
-//           {/*      height="20"*/}
-//           {/*      transform="translate(-10, -10)" // Center the image on the coordinates*/}
-//           {/*    />*/}
-//           {/*  </Marker>*/}
-//           {/*))}*/}
-//         </ZoomableGroup>
-//       </ComposableMap>
-//     </>
-//   );
-// };
-//
-// export default MapWorld3;
-
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, ZoomableGroup, Graticule, Sphere } from "react-simple-maps";
+import { useRouter } from 'next/navigation';
 
 const geoUrl = "/features.json";
 
@@ -191,67 +15,100 @@ const getRandomColor = () => {
   return color;
 };
 
+interface GeoProperties {
+  name: string;
+}
+
+interface GeoType {
+  properties: GeoProperties;
+}
+
+interface DataType {
+  objects: {
+    world: {
+      geometries: GeoType[];
+    };
+  };
+}
+
 const MapWorld3 = () => {
-  const [colors, setColors] = useState({});
+  const router = useRouter();
+
+  const [colors, setColors] = useState<{ [key: string]: string }>({});
+
+  const handleClick = (geo: GeoType) => () => {
+    const region = geo.properties.name;
+    const id = region; // Assuming 'id' is the region name, update this if needed
+    console.log(region, id);
+    router.push(`/stories?region=${region}&id=${id}`);
+  };
+
+  const getColorCountries = (newColors: { [key: string]: string }, regionName: string) => {
+    newColors[regionName] = getRandomColor();
+    setColors({ ...newColors });
+  };
 
   useEffect(() => {
     const countRandomColor = 3;
     fetch(geoUrl)
-      .then(response => response.json())
-      .then(data => {
-        const newColors = {};
-        data.objects?.world?.geometries.forEach((geo) => {
+      .then((response) => response.json())
+      .then((data: DataType) => {
+        const newColors: { [key: string]: string } = {};
+        data.objects?.world?.geometries.forEach((geo: GeoType) => {
           const regionName = geo.properties.name;
-          const randomColor = getRandomColor();
-          newColors[regionName] = randomColor;
-          setColors(newColors);
+          getColorCountries(newColors, regionName);
           for (let i = 1; i <= countRandomColor; i++) {
             setTimeout(() => {
-              const randomColor = getRandomColor();
-              newColors[regionName] = randomColor;
-              setColors(newColors);
+              getColorCountries(newColors, regionName);
             }, 1000 * i);
           }
         });
-        //setColors(newColors);
       });
   }, []);
 
   return (
-    <>
-      <ComposableMap
-        projectionConfig={{
-          rotate: [-10, 0, 0],
-          scale: 147,
-        }}
-        width={800}
-        height={400}
-        style={{ backgroundColor: "#F0F8FF" }}
-      >
-        <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => {
-                const regionName = geo.properties.name;
-                const fillColor = colors[regionName] || "#87CEEB";
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill={fillColor}
-                    style={{
-                      default: { outline: "none" },
-                      hover: { outline: "none", fill: "#6495ED" },
-                      pressed: { outline: "none", fill: "#1E90FF" },
-                    }}
-                  />
-                );
-              })
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </>
+    <ComposableMap
+      projectionConfig={{
+        rotate: [-10, 0, 0],
+        scale: 147,
+        center: [0, -30]
+      }}
+      style={{ backgroundColor: "#F0F8FF" }}
+    >
+      <Sphere stroke="#E4E5E6" strokeWidth={0.6} id='1' fill='#87CEEB' />
+      <Graticule stroke="#E4E5E6" strokeWidth={0.6} id='2' fill='#87CEEB' />
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              const regionName = geo.properties.name;
+              const fillColor = colors[regionName] || "#87CEEB";
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={fillColor}
+                  onClick={handleClick(geo)}
+                  style={{
+                    default: {
+                      outline: "none",
+                      cursor: "pointer",
+                    },
+                    hover: {
+                      outline: "none",
+                      fill: "gold",
+                      cursor: "pointer",
+                    },
+                    pressed: {
+                      outline: "none",
+                      fill: "#1E90FF",
+                    },
+                  }}
+                />
+              );
+            })
+          }
+        </Geographies>
+    </ComposableMap>
   );
 };
 
