665cac3f10e767c9de61220f354637fee69e6d21
Roman Kravets <romann.kravets@gmail.com>
2026-03-09 18:04:50 +0200

    add

--- src/db/firebaseAdmin.ts
+++ src/db/firebaseAdmin.ts
@@ -24,7 +24,10 @@ function getFirebaseAdmin() {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
         clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
+          /\\n/g,
+          "\n",
+        ),
       }),
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     });
